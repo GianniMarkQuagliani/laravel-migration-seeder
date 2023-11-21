3a9571199046b06a017da76d5b7b6d779a640fc5
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-22 00:58:20 +0100

    add data on database

--- database/migrations/2023_11_21_153734_create_trains_table.php
+++ database/migrations/2023_11_21_153734_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda',20);
+            $table->string('Stazione_di_partenza',20);
+            $table->string('Stazione_di_arrivo',20);
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->string('Codice_Treno',10);
+            $table->unsignedTinyInteger('Numero_Carrozze');
+            $table->boolean('In_orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
             $table->timestamps();
         });
     }
